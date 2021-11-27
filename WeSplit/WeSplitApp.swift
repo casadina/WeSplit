e8b9a5717fd3f89f68967b8b18bff507b1d3f319
James Blalock <casadina@tuta.io>
2021-11-27 21:04:30 +0800

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by aeglus on 2021/11/27.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
